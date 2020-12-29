768e4034d43715eb79fd8c4a2db583eef46149c1
Desenvolvimento 04 <desenvolvimento04@servidordell.inspell.com.br>
2020-12-29 10:33:05 -0300

    exbibindo erros

--- frontend/README.md
+++ frontend/README.md
@@ -4,3 +4,6 @@ yarn add styled-components
 yarn add @types/styled-components -D
 yarn add react-icons
 yarn add polished
+yarn add @unform/core @unform/web
+yarn add yup
+yarn add @types/yup -D
--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { isSchema } from 'yup';
 
-const SingUp: React.FC = () => (
-           <Container>
+const SingUp: React.FC = () => {
+
+      const handleSubmit = useCallback(async (data: object) => {
+          
+          try {
+            const schema = Yup.object().shape({
+              name: Yup.string().required('Nome é obrigatório'),
+              email: Yup.string().required('E-mail é obrigatório').email('Digite um e-mail válido'),
+              password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+            });
+
+            await schema.validate(data, {
+              abortEarly: false
+            });
+
+          } catch (error) {
+            console.log(error);
+            
+          }
+          
+        
+        }, []);
+
+
+        return( 
+            <Container>
              <Background />
-             <Content>
-                <img src={logoImg} alt=""/>
-
-                <form>
-                    <h1>Faça seu cadastro</h1>
+               <Content>
+                  <img src={logoImg} alt=""/>
+  
+                  <Form onSubmit={handleSubmit}>
+                      <h1>Faça seu cadastro</h1>
 
                     <Input name="name" icon={FiUser} placeholder="Nome"/>
 
@@ -22,7 +49,7 @@ const SingUp: React.FC = () => (
 
                     <Button type="submit">Cadastrar</Button>
 
-                </form>
+                  </Form>
 
                 <a href="/">
                     <FiArrowLeft />
@@ -31,7 +58,7 @@ const SingUp: React.FC = () => (
 
              
            </ Container>
-);
+        );};
 
 
 export default SingUp;
